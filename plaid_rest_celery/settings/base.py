0f10d025dc01b27ff455486ba4e2909768d1cea2
Jay Modi <mjrulesamrat@gmail.com>
2020-03-19 00:13:26 +0530

    Add REST TokenAuthentication class

--- plaid_rest_celery/settings/base.py
+++ plaid_rest_celery/settings/base.py
@@ -113,6 +113,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_RENDERER_CLASSES': [
