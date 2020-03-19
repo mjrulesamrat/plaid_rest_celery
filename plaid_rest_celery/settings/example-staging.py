e240355a56d8d21cc72fd4d9ae66009497eff593
Jay Modi <mjrulesamrat@gmail.com>
2020-03-20 02:53:10 +0530

    Apply prometheus sqlite3 backend engine

--- plaid_rest_celery/settings/example-staging.py
+++ plaid_rest_celery/settings/example-staging.py
@@ -15,6 +15,7 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", False)
 
 DATABASES = {
     "default": {
+        # "ENGINE": 'django_prometheus.db.backends.sqlite3',
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER", "user"),
