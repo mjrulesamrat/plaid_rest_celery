6d94b6485b53f1bb2045466e3dc8d6f1d47f680f
Jay Modi <mjrulesamrat@gmail.com>
2020-03-27 12:45:57 +0530

    Add ToDo: public_token validation

--- plaidapp/forms.py
+++ plaidapp/forms.py
@@ -3,4 +3,8 @@ from django import forms
 
 
 class PublicTokenForm(forms.Form):
+    """
+    Validations here:
+    https://plaid.com/docs/#plaid-tokens-public_token-access_token-or-asset_report_token
+    """
     public_token = forms.CharField(required=True)
